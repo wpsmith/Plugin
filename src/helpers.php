b83a1c1eec8005f9a73a2f4d7eaf4f6532e02d72
wpsmith <t@wpsmith.net>
2020-03-16 21:16:09 -0400

    Add helper functions

--- src/helpers.php
+++ src/helpers.php
@@ -23,3 +23,51 @@ if ( ! function_exists( __NAMESPACE__ . '\is_plugin_active' ) ) {
 		return \is_plugin_active( $plugin );
 	}
 }
+
+if ( ! function_exists( __NAMESPACE__ . '\extend_plugins' ) ) {
+	/**
+	 * Extends multiple plugins.
+	 *
+	 * <code>
+	 * $array = array(
+	 *   'plugin_path/plugin_root_file.php'   => '1.0.0',
+	 *   'advanced-custom-fields-pro/acf.php' => '5.8.7',
+	 * );
+	 * </code>
+	 *
+	 * @param array[string]string $plugins Array of plugin basename => version.
+	 * @param string $file
+	 * @param string $text_domain
+	 */
+	function extend_plugins( $plugins, $file, $text_domain = 'wps' ) {
+		foreach ( $plugins as $plugin => $version ) {
+			new \WPS\WP\Plugin\ExtendPlugin( $plugin, $file, $version, $text_domain );
+		}
+	}
+}
+
+if ( ! function_exists( __NAMESPACE__ . '\hide_plugins' ) ) {
+	/**
+	 * Hides an array of plugins.
+	 *
+	 * @param array $plugins Array of plugin basenames.
+	 */
+	function hide_plugins( $plugins ) {
+		foreach ( $plugins as $plugin ) {
+			new \WPS\WP\Plugin\HidePlugin( $plugin );
+		}
+	}
+}
+
+if ( ! function_exists( __NAMESPACE__ . '\prevent_plugin_updates' ) ) {
+	/**
+	 * Prevents an array of plugins from updating.
+	 *
+	 * @param array $plugins Array of plugin basenames.
+	 */
+	function prevent_plugin_updates( $plugins ) {
+		foreach ( $plugins as $plugin ) {
+			new \WPS\WP\Plugin\PreventUpdate( $plugin );
+		}
+	}
+}
