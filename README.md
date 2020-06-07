d07c9729475489c62433422e2ae82f5cc632f4fc
wpsmith <t@wpsmith.net>
2020-06-07 16:46:49 -0400

    Update Readme; add UninstallManager

--- README.md
+++ README.md
@@ -18,6 +18,14 @@ These classes solve three (3) particular problems:
 1. _Extending Plugins_
 
     Many core functionality plugins extend/modify other plugins. If the dependency plugin that is being extended is deactivated, then the plugin makes the WordPress instance unstable and breaks the whole site.
+    
+1. _Uninstalling Plugins_
+
+    It is a best practice to always uninstall your plugin data and files. However, an interface to determine what should be uninstalled needs to be built. This uses WP Pointers to create such an interface giving the user the choice to uninstall just the plugin files or everything.
+
+1. _Building Plugins_
+
+    When building a plugin, this library provides a base class to be extended as the new plugins core class.
 
 
 ## Installation
@@ -39,33 +47,172 @@ or:
 ### Update Prevention
 
 To prevent the current plugin from updating:
-~~~php
+```php
 new \WPS\WP\Plugin\PreventUpdate( plugin_basename( __FILE__ ) );
-~~~
+```
 
 To prevent another plugin (e.g., ACF) from updating:
-~~~php
+```php
 new \WPS\WP\Plugin\PreventUpdate( 'advanced-custom-fields-pro/acf.php' );
-~~~
+```
 
 ### Hiding Plugin(s)
 
 To hide the current plugin:
-~~~php
+```php
 new \WPS\WP\Plugin\HidePlugin( plugin_basename( __FILE__ ) );
-~~~
+```
 
 To hide another plugin (e.g., ACF):
-~~~php
+```php
 new \WPS\WP\Plugin\HidePlugin( 'advanced-custom-fields-pro/acf.php' );
-~~~
+```
 
 ### Extending Plugin(s)
 
 To extend ACF, you would do something like this:
-~~~php
+```php
 new \WPS\WP\Plugin\ExtendPlugin( 'advanced-custom-fields-pro/acf.php', __FILE__, '5.8.7', 'plugin-text-domain' );
-~~~
+```
+
+### Uninstalling Plugin(s)
+
+To use the `UninstallManager`, you would do something like this at the plugin's root:
+
+#### Simple example
+Within the plugin's main file:
+```php
+global $my_plugin_uninstaller;
+$my_plugin_uninstaller = new \WPS\WP\Plugin\UninstallManager( __FILE__ );
+
+// Register activation stuffs.
+register_activation_hook( __FILE__, function() {
+    global $my_plugin_uninstaller;
+    UninstallManager::on_activation( $my_plugin_uninstaller );
+} );
+
+// Register deactivation stuffs.
+register_deactivation_hook( __FILE__, function() {
+    global $my_plugin_uninstaller;
+    UninstallManager::on_deactivation( $my_plugin_uninstaller );
+} );
+
+// If not using an uninstall.php, you need to register uninstall stuffs.
+register_uninstall_hook( __FILE__, function() {
+    global $my_plugin_uninstaller;
+    $wps_codeable_delete_action = $my_plugin_uninstaller->get_uninstall_action();
+    
+    // Bail if not deleting everything.
+    if ( 'everything' !== $wps_codeable_delete_action ) {
+    	return;
+    }
+    
+    // Delete Options.
+    $my_plugin_uninstaller->uninstall();
+} );
+```
+
+#### A Better Example
+Within the main plugin file:
+```php
+/**
+ * Gets uninstall manager.
+ *
+ * This function can be placed anywhere. Alternatively, you can use a global variable to hold the uninstaller.
+ * 
+ * @param string $plugin_file Absolute path to plugin base file.
+ *
+ * @return \WPS\WP\Plugin\UninstallManager
+ */
+function get_uninstall_manager( $plugin_file ) {
+    static $mgr;
+    if ( null === $mgr ) {
+        $mgr = new \WPS\WP\Plugin\UninstallManager( $plugin_file );
+    }
+
+    return $mgr;
+}
+
+// Setup Uninstall Manager at base of the plugin.
+get_uninstall_manager( __FILE__ );
+
+register_activation_hook( __FILE__, function() {
+    UninstallManager::on_activation( get_uninstall_manager() );
+} );
+register_deactivation_hook( __FILE__, function() {
+    UninstallManager::on_deactivation( get_uninstall_manager() );
+} );
+```
+
+Then in `uninstall.php`:
+```php
+// Require the main plugin file.
+require_once( 'plugin.php' );
+
+// Get what we are supposed to do on deletion/uninstall.
+$uninstall_manager = get_uninstall_manager();
+$wps_codeable_delete_action = $uninstall_manager->get_uninstall_action();
+
+// Bail if not deleting everything.
+if ( 'everything' !== $wps_codeable_delete_action ) {
+	return;
+}
+
+// Delete Options.
+$uninstall_manager->uninstall();
+```
+### Building a Plugin
+
+For all my plugins, I build a base class for the main plugin. Out of the box, `PluginBase` does three things:
+1. Loads plugin's text domain accordingly.
+1. Implements `Singleton` so there can only be one.
+1. Provides the following methods:
+  * `Plugin::doing_ajax()` or `PluginBase::doing_ajax()` - determines whether WP is processing an AJAX request.
+  * `Plugin::get_version()` - gets the plugin's version. Defaults to `0.0.0`.
+  * `Plugin::get_plugin_name()` - gets the plugin's name. Defaults to `wps`.~~~~
+  * `$this->get_template_loader()` - gets the plugin's template loader. See [TemplateLoader](https://github.com/wpsmith/templates). 
+  * `$this->add_action()` - Either adds the action to the correct hook or performs the action if the hook has already been done or is being done. 
+
+Generally speaking, it goes something like this:
+```php
+namespace WPS\WP\Plugins\MyPlugin;
+
+use WPS\WP\Plugin\PluginBase;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( __NAMESPACE__ . '\Plugin' ) ) {
+	/**
+	 * Class Plugin
+	 *
+	 * @package \WPS\WP\Plugins\MyPlugin
+	 */
+	class Plugin extends PluginBase {
+	    /**
+         * Plugin constructor.
+         *
+         * @param array $args Optional args.
+         *
+         * @since 0.0.1
+         */
+        protected function __construct( $args = array() ) {
+            // Construct the parent.
+            parent::__construct( $args );
+        }
+    }
+}
+
+// Instantiate.
+Plugin::get_instance( array(
+    'name'    => 'my-plugin',
+    'version' => '0.0.1',
+    'file'    => __FILE__,
+) );
+```
+
 
 ## Change Log
 
---
+++ src/UninstallManager.php
@@ -0,0 +1,249 @@
+<?php
+
+/**
+ * WPS Plugin Data Manager Class
+ *
+ * @package    WPS\WP\Plugin
+ * @author     Travis Smith <t@wpsmith.net>
+ * @copyright  2015-2018 WP Smith, Travis Smith
+ * @link       https://github.com/wpsmith/WPS/
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @version    0.0.1
+ * @since      0.0.1
+ */
+
+namespace WPS\WP\Plugins\Codeable;
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( __NAMESPACE__ . '\UninstallManager' ) ) {
+	/**
+	 * Plugin Uninstall Manager Class
+	 *
+	 * @package WPS\WP\Plugin
+	 * @author Travis Smith <t@wpsmith.net>
+	 */
+	class UninstallManager {
+
+		/**
+		 * Plugin slug.
+		 *
+		 * @var string
+		 */
+		protected string $plugin_slug;
+
+		/**
+		 * Plugin basename.
+		 *
+		 * @var string
+		 */
+		protected string $plugin_file;
+
+		/**
+		 * Default delete action is to remove plugin only.
+		 *
+		 * @var string
+		 */
+		public static string $default_delete_action = 'plugin-only';
+
+		/**
+		 * DataManager constructor.
+		 *
+		 * @param string $plugin_file Plugin file.
+		 * @param string $plugin_slug Plugin slug.
+		 */
+		public function __construct( $plugin_file, $plugin_slug ) {
+
+			$this->plugin_file = $plugin_file;
+			$this->plugin_slug = sanitize_title_with_dashes( $plugin_slug );
+
+			add_action( 'admin_footer', array( $this, 'delete_data_or_plugin' ) );
+
+		}
+
+		/**
+		 * Default deletion actions.
+		 *
+		 * @return array {
+		 *      Optional. An array of valid/supported delete actions. Default empty array.
+		 *
+		 * @type array  $id {
+		 *          Optional. Valid deletion action.
+		 * @type string $button Button class. Default ''. Accepts 'primary', 'secondary', or ''.
+		 * @type string $text Text of the button.
+		 *      }
+		 * }
+		 */
+		protected static function get_default_delete_actions() {
+			return array(
+				'plugin-only' => self::create_delete_action( __( 'Delete Plugin Only', 'wps' ), 'primary' ),
+				'everything'  => self::create_delete_action( __( 'Delete Everything', 'wps' ) ),
+			);
+		}
+
+		/**
+		 * Creates delete action.
+		 *
+		 * @param string $button_text Button text.
+		 * @param string $button_class Button class.
+		 *
+		 * @return array $args {
+		 * @type string  $button Button class. Default ''. Accepts 'primary', 'secondary', or ''.
+		 * @type string  $name Text of the button.
+		 * }
+		 */
+		protected static function create_delete_action( $button_text, $button_class = '' ) {
+			$accepted_button_classes = array(
+				'primary',
+				'secondary',
+			);
+
+			if ( in_array( $button_class, $accepted_button_classes, true ) ) {
+				return array(
+					'button' => $button_class,
+					'text'   => $button_text,
+				);
+			}
+
+			return array(
+				'button' => '',
+				'text'   => $button_text,
+			);
+		}
+
+		/**
+		 * Gets the valid/supported deletion actions.
+		 *
+		 * @return string
+		 */
+		protected function get_delete_actions_content() {
+			$default_action = $this->get_uninstall_action();
+
+			$actions = array();
+			$content = sprintf(
+				'<h3>%s</h3><p>',
+				__( 'On delete, delete the plugin or delete everything?', 'wps' )
+			);
+			foreach ( self::get_default_delete_actions() as $id => $delete_action ) {
+				if ( '' !== $default_action ) {
+					if ( $id === $default_action ) {
+						$delete_action['button'] = 'primary';
+					} else {
+						$delete_action['button'] = '';
+					}
+				}
+				$actions[] = sprintf(
+					'<a id="%1$s" class="button%2$s" href="\' + $(e.currentTarget).attr(\'href\') + \'&delete_action=%1$s">%3$s</a>',
+					$id,
+					'' !== $delete_action['button'] ? ' button-' . $delete_action['button'] : '',
+					$delete_action['text']
+				);
+			}
+			$content .= join( '&nbsp;', $actions );
+			$content .= '</p>';
+
+			return $content;
+		}
+
+		/**
+		 * Adds a pointer and event on plugins page.
+		 */
+		public function delete_data_or_plugin() {
+			if ( ! self::is_plugins_page() ) {
+				return;
+			}
+
+			wp_enqueue_style( 'wp-pointer' );
+			wp_enqueue_script( 'wp-pointer' );
+			wp_enqueue_script( 'utils' ); // for user settings
+			?>
+            <script type="text/javascript">
+                (function ($) {
+                    var selector = 'tr[data-slug="<?php echo $this->plugin_slug; ?>"]';
+                    $(selector + ' .delete a, ' + selector + ' .deactivate a').click(function (e) {
+                        $(e.currentTarget).pointer({
+                            content: '<?php echo $this->get_delete_actions_content(); ?>',
+                            position: {
+                                my: 'left top',
+                                at: 'center bottom',
+                                offset: '-1 0'
+                            },
+                            close: function () {
+                                // Person just closed the pointer, prevent moving forward.
+                                e.preventDefault();
+                            }
+                        }).pointer('open');
+                        return false;
+                    });
+                })(jQuery);
+            </script>
+			<?php
+		}
+
+		/**
+		 * Determines whether the current admin page is the plugins page.
+		 *
+		 * @return bool
+		 */
+		public static function is_plugins_page() {
+			global $pagenow;
+			$screen = get_current_screen();
+
+			return is_admin() && ( 'plugins.php' === $pagenow || null !== $screen && 'plugins.php' === $screen->base );
+		}
+
+		/**
+		 * Deactivation function.
+		 *
+		 * To be called as part of the deactivation hook.
+		 *
+		 * @param UninstallManager $instance The uninstall manager.
+		 */
+		public static function on_deactivation( UninstallManager $instance ) {
+
+			if (
+				isset( $_GET['delete_action'] ) &&
+				in_array( $_GET['delete_action'], array_keys( self::get_default_delete_actions() ), true )
+			) {
+				update_option( $instance->plugin_slug . '_delete_action', $_GET['delete_action'], false );
+			}
+
+		}
+
+		/**
+		 * Activation function.
+		 *
+		 * To be called as part of the activation hook.
+		 *
+		 * @param UninstallManager $instance The uninstall manager.
+		 */
+		public static function on_activation( UninstallManager $instance ) {
+
+			add_option( $instance->plugin_slug . '_delete_action', self::$default_delete_action, null, false );
+
+		}
+
+		/**
+		 * Uninstall/Delete function.
+		 */
+		public function uninstall() {
+
+			delete_option( $this->plugin_slug . '_delete_action' );
+
+		}
+
+		/**
+		 * Gets the uninstall/delete action.
+		 *
+		 * @return string Either: 'plugin-only' or 'everything'.
+		 */
+		public function get_uninstall_action() {
+
+			return get_option( $this->plugin_slug . '_delete_action', self::$default_delete_action );
+
+		}
+	}
+}
